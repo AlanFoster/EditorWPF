ade4cf5fad003b3f95ea832620a760aaea658255
Alan Foster <alan@alanfoster.me>
2013-09-25 21:20:46 +0100

    Update csproj (msbuild) to generate a single combined exe

---
+++ EditorWPF/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+
+namespace EditorWPF
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += OnResolveAssembly;
+            App.Main();
+        }
+
+        // http://www.digitallycreated.net/Blog/61/combining-multiple-assemblies-into-a-single-exe-for-a-wpf-application
+        private static Assembly OnResolveAssembly(object sender, ResolveEventArgs args)
+        {
+            var executingAssembly = Assembly.GetExecutingAssembly();
+            var assemblyName = new AssemblyName(args.Name);
+
+            var path = assemblyName.Name + ".dll";
+            if (assemblyName.CultureInfo.Equals(CultureInfo.InvariantCulture) == false)
+            {
+                path = String.Format(@"{0}\{1}", assemblyName.CultureInfo, path);
+            }
+
+            using (var stream = executingAssembly.GetManifestResourceStream(path))
+            {
+                if (stream == null)
+                    return null;
+
+                var assemblyRawBytes = new byte[stream.Length];
+                stream.Read(assemblyRawBytes, 0, assemblyRawBytes.Length);
+                return Assembly.Load(assemblyRawBytes);
+            }
+        }
+    }
+}
